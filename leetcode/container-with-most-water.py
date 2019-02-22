fa8bb46921d4891c6853830cc5b7fdace49cfb37
Sumin Byeon <suminb@gmail.com>
2019-02-23 02:06:07 +0900

    11. Container With Most Water

---
+++ leetcode/container-with-most-water.py
@@ -0,0 +1,32 @@
+# class Solution:
+#     def maxArea(self, height: 'List[int]') -> 'int':
+#         max_area = 0
+#         for i, h1 in enumerate(height):
+#             for j, h2 in enumerate(height[i + 1:], i + 1):
+#                 area = (j - i) * min(h1, h2)
+#                 max_area = max(area, max_area)
+#
+#         return max_area
+
+#
+# Brute force solution timed out:
+# https://leetcode.com/submissions/detail/209829829/
+#
+
+class Solution:
+    def maxArea(self, height: 'List[int]') -> 'int':
+        max_area = 0
+        i, j = 0, len(height) - 1
+        while i < j:
+            h1, h2 = height[i], height[j]
+            area = (j - i) * min(h1, h2)
+            max_area = max(max_area, area)
+
+            if h1 < h2:
+                i += 1
+            else:
+                j -= 1
+
+        return max_area
+
+# https://leetcode.com/submissions/detail/209834037/
