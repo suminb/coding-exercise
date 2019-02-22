5509516a572856417eae0e4e2a71f5638e127f85
Sumin Byeon <suminb@gmail.com>
2019-02-23 02:20:59 +0900

    Solution without using built-in functions

--- leetcode/container-with-most-water.py
+++ leetcode/container-with-most-water.py
@@ -30,3 +30,25 @@ class Solution:
         return max_area
 
 # https://leetcode.com/submissions/detail/209834037/
+
+
+# class Solution:
+#     def maxArea(self, height: 'List[int]') -> 'int':
+#         max_area = 0
+#         i, j = 0, len(height) - 1
+#         while i < j:
+#             h1, h2 = height[i], height[j]
+#             area = (j - i) * (h1 if h1 < h2 else h2)
+#             if area > max_area:
+#                 max_area = area
+
+#             if h1 < h2:
+#                 i += 1
+#             else:
+#                 j -= 1
+
+#         return max_area
+
+# NOTE: It is somewhat faster without using min() and max() built-in functions
+# https://leetcode.com/submissions/detail/209836084/
+
