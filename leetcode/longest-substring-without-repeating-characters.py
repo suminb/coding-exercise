2d1ebc13c865d21525a701a174ab05987a470a40
Sumin Byeon <suminb@gmail.com>
2019-02-24 22:13:19 +0900

    Sliding window solution

--- leetcode/longest-substring-without-repeating-characters.py
+++ leetcode/longest-substring-without-repeating-characters.py
@@ -1,24 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, xs: str) -> int:
-        i, n = 0, len(xs)
+        i, j, n, s = 0, 0, len(xs), set()
         max_len = 0
 
-        while i < n:
-            max_len = max(max_len, self.longest_substr(xs[i:]))
-            i += 1
-
-        return max_len
-
-    def longest_substr(self, xs):
-        i, n, s = 0, len(xs), set()
-        max_len = 0
-
-        while i < n:
-            s.add(xs[i])
-
-            if len(s) != n - i:
-                return max_len
+        while i < n and j < n:
+            if xs[j] in s:
+                s.remove(xs[i])
+                i += 1
             else:
-                max_len = n - i
+                s.add(xs[j])
+                max_len = max(max_len, len(s))
+                j += 1
 
         return max_len
