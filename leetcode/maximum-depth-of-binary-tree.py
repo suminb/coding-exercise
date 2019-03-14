1a1af64f713d087054921707ebf440766b9a09fb
Sumin Byeon <suminb@gmail.com>
2019-03-15 02:15:36 +0900

    Add test cases

--- leetcode/maximum-depth-of-binary-tree.py
+++ leetcode/maximum-depth-of-binary-tree.py
@@ -1,7 +1,7 @@
 # 104. Maximum Depth of Binary Tree
 # difficulty: easy
 
-from common import TreeNode
+from common import build_binary_tree, TreeNode
 
 
 class Solution:
@@ -13,3 +13,19 @@ class Solution:
             return max(self.max_depth(node.left, depth + 1), self.max_depth(node.right, depth + 1))
         else:
             return depth
+
+
+def test():
+    s = Solution()
+
+    root = build_binary_tree([])
+    assert s.maxDepth(root) == 0
+
+    root = build_binary_tree([0])
+    assert s.maxDepth(root) == 1
+
+    root = build_binary_tree([1, 2, 3])
+    assert s.maxDepth(root) == 2
+
+    root = build_binary_tree([3, 9, 20, None, None, 15, 7])
+    assert s.maxDepth(root) == 3
