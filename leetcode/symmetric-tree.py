5bb4f513549d5442666a708269fd44c5e198caf6
Sumin Byeon <suminb@gmail.com>
2019-03-15 22:07:37 +0900

    Simple recursive solution

--- leetcode/symmetric-tree.py
+++ leetcode/symmetric-tree.py
@@ -8,70 +8,35 @@ from common import TreeNode
 
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        return is_symmetric(root)
+        return is_mirror(root, root)
 
 
-def is_symmetric(root):
-    prev_depth = -1
-    queue = deque([(root, 0)])
-    single_level = []
-    while queue:
-        node, depth = queue.popleft()
-
-        print(depth, single_level)
-
-        if depth != prev_depth:
-            if not is_symmetric_list(single_level, prev_depth):
-                return False
-            single_level = []
-            prev_depth = depth
-        single_level.append(node.val if node else None)
-
-        if node:
-            queue.append((node.left, depth + 1))
-            queue.append((node.right, depth + 1))
-
-    if not is_symmetric_list(single_level, depth):
-        return False
-
-    return True
-
-
-def is_symmetric_list(xs, depth):
-    if depth < 0:
+def is_mirror(node1, node2):
+    if node1 is None and node2 is None:
         return True
-
-    # expected length = 2 ** depth
-    n = 2 ** depth
-    mid = n // 2
-    return len(xs) == n and all([x == y for x, y in zip(xs[:mid], xs[n:mid:-1])])
-
-
-def test_is_symmetric_list():
-    assert is_symmetric_list([], -1)
-    assert is_symmetric_list([1], 0)
-    assert is_symmetric_list([1, 1], 1)
-    assert is_symmetric_list([1, 2, 2, 1], 2)
-    assert is_symmetric_list([1, None, None, 1], 2)
-
-    assert not is_symmetric_list([1, 2, 1, 2], 2)
+    if node1 is None or node2 is None:
+        return False
+    return node1.val == node2.val and \
+            is_mirror(node1.left, node2.right) and \
+            is_mirror(node1.right, node2.left)
 
 
 def test_is_symmetric(build_binary_tree):
+    s = Solution()
     root = build_binary_tree([])
-    assert is_symmetric(root)
+    assert s.isSymmetric(root)
 
     root = build_binary_tree([1])
-    assert is_symmetric(root)
+    assert s.isSymmetric(root)
 
     root = build_binary_tree([1, 2, 2, 3, 4, 4, 3])
-    assert is_symmetric(root)
+    assert s.isSymmetric(root)
 
     root = build_binary_tree([1, 2])
-    assert not is_symmetric(root)
+    assert not s.isSymmetric(root)
 
     root = build_binary_tree([1, 2, 3])
-    assert not is_symmetric(root)
+    assert not s.isSymmetric(root)
 
     root = build_binary_tree([1, 2, 2, None, 3, None, 3])
-    assert not is_symmetric(root)
+    assert not s.isSymmetric(root)
