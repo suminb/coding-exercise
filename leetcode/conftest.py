cde39788ab7a9e285390bf2674411d7c41a45df8
Sumin Byeon <suminb@gmail.com>
2019-03-16 23:15:46 +0900

    Make build_binary_tree as a fixture

--- leetcode/conftest.py
+++ leetcode/conftest.py
@@ -1,8 +1,15 @@
 from pytest import fixture
 
+from common import build_binary_tree as build_binary_tree_, TreeNode
+
 
 @fixture
 def assert_list_equals():
     def func(xs, ys):
         assert set(tuple([tuple(x) for x in xs])) == set(tuple([tuple(y) for y in ys]))
     return func
+
+
+@fixture
+def build_binary_tree():
+    return build_binary_tree_
