6fc54e6f94ec7b054ed898a9df054a8eba06cbcf
Sumin Byeon <suminb@gmail.com>
2019-03-23 23:15:46 +0900

    Getting started with Golang

---
+++ goex/fetch.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"golang.org/x/text/encoding"
+	"io"
+	"net/http"
+)
+
+// This is a collection of functions to fetch remove resources such as web pages and structured data provided by APIs.
+
+
+// Fetch sends out an HTTP GET request and returns an HTTP response
+func Fetch(url string, params map[string]string, headers map[string]string) (*http.Response, error) {
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+
+	// TODO: Fill in query strings
+	//for k, v := range params {
+	//}
+
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	res, err := client.Do(req)
+	return res, err
+}
+
+func ForceConvertEncoding(reader io.Reader, encoding encoding.Encoding) io.Reader {
+	decoder := encoding.NewDecoder()
+	return decoder.Reader(reader)
+}
---
+++ goex/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/text/encoding/korean"
+)
+
+func ExampleScrape() {
+
+	reader := FetchSampleFile("marketindex.html")
+	reader = ForceConvertEncoding(reader, korean.EUCKR)
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	elements := doc.Find(".tbl_exchange tr")
+	elements.Each(func(i int, s *goquery.Selection) {
+		label := s.Find("span")
+		if label.Text() == "국고채 (3년)" {
+			row := label.Parent().Parent().Parent()
+			cell := row.Children()
+
+			value := strings.TrimSpace(cell.Eq(1).Text())
+			delta := strings.TrimSpace(cell.Eq(2).Text())
+
+			fmt.Printf("value = %s, delta = %s\n", value, delta)
+		}
+	})
+}
+
+func GetReaderWithURL(url string) io.Reader {
+	// Request the HTML page.
+	resp, _ := Fetch("https://finance.naver.com/marketindex/", make(map[string]string), make(map[string]string))
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	return resp.Body
+}
+
+func FetchSampleFile(filename string) io.Reader {
+	file, _ := os.Open(filename)
+	return bufio.NewReader(file)
+}
+
+func main() {
+	ExampleScrape()
+}
