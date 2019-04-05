c58202b8bf01cdde46ad949bc30397482ecb275e
Sumin Byeon <suminb@gmail.com>
2019-04-06 05:48:59 +0900

    Prototyping rebalancing algorithm

--- goex/src/finance/finance.go
+++ goex/src/finance/finance.go
@@ -10,6 +10,12 @@ type PortfolioRecord struct {
 	DesiredShare float64
 }
 
+// RebalancePlan indicates how much of the asset must be purchased or sold
+type RebalancePlan struct {
+	Asset    Asset
+	Quantity float64
+}
+
 // NOTE: Not sure if taking *Portfolio as a parameter, but couldn't think of a better way
 func (record *PortfolioRecord) CurrentShare(portfolio *Portfolio) float64 {
 	nav := portfolio.CalcNetAssetValue()
@@ -39,6 +45,18 @@ func (portfolio *Portfolio) CalcNetAssetValue() float64 {
 	return netAssetValue
 }
 
-func Rebalance() {
+// Rebalance a given portfolio according to the desired shares of each asset
+// TODO: Need an option to determine whether we want to allow non-discrete quantities
+func (portfolio *Portfolio) Rebalance() []RebalancePlan {
+	plans := make([]RebalancePlan, len(portfolio.Records))
+	nav := portfolio.CalcNetAssetValue()
+	for i, record := range portfolio.Records {
+		unitPrice := record.Asset.UnitPrice
+		currentValue := unitPrice * record.Quantity
+		targetValue := record.DesiredShare * nav
 
+		plans[i].Asset = record.Asset
+		plans[i].Quantity = (targetValue - currentValue) / unitPrice
+	}
+	return plans
 }
--- goex/src/finance/finance_test.go
+++ goex/src/finance/finance_test.go
@@ -1,13 +1,14 @@
 package finance
 
 import (
+	"fmt"
 	"math"
 	"testing"
 )
 
 const delta = 0.000001
 
-var portfolio Portfolio
+var portfolio, portfolio2 Portfolio
 
 func init() {
 	records := []PortfolioRecord{
@@ -15,8 +16,14 @@ func init() {
 		{Asset: Asset{Name: "AMD", Type: Stock, UnitPrice: 28}, Quantity: 1480},
 		{Asset: Asset{Name: "AMZN", Type: Stock, UnitPrice: 1850}, Quantity: 100},
 	}
-
 	portfolio = Portfolio{Records: records}
+
+	records = []PortfolioRecord{
+		{Asset: Asset{Name: "Stock", Type: Stock, UnitPrice: 1000}, Quantity: 100, DesiredShare: 0.3},
+		{Asset: Asset{Name: "Bond", Type: Bond, UnitPrice: 1000}, Quantity: 100, DesiredShare: 0.4},
+		{Asset: Asset{Name: "Cash", Type: Cash, UnitPrice: 1000}, Quantity: 100, DesiredShare: 0.3},
+	}
+	portfolio2 = Portfolio{Records: records}
 }
 
 func TestPortfolio_CalcNetAssetValue(t *testing.T) {
@@ -37,6 +44,9 @@ func TestPortfolioRecord_CurrentShare(t *testing.T) {
 	}
 }
 
-func TestRebalance(t *testing.T) {
-
+func TestPortfolio_Rebalance(t *testing.T) {
+	plans := portfolio2.Rebalance()
+	for _, p := range plans {
+		fmt.Printf("%s %f\n", p.Asset.Name, p.Quantity)
+	}
 }
