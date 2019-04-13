2b13a5f70f70100657eb073821481a5fb87f2ad2
Sumin Byeon <suminb@gmail.com>
2019-04-14 01:27:52 +0900

    Recursive solution

--- leetcode/remove-nth-node-from-end-of-list.go
+++ leetcode/remove-nth-node-from-end-of-list.go
@@ -10,18 +10,19 @@ type ListNode struct {
 }
 
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	newHead, m := remove(head, n)
-	// FIXME: Perhaps we could do it without this kind of special case handling
-	if n == m {
-		return newHead.Next
-	} else {
-		return newHead
-	}
+	return removeIteratively(head, n)
+	// newHead, m := removeRecursively(head, n)
+	// // FIXME: Perhaps we could do it without this kind of special case handling
+	// if n == m {
+	// 	return newHead.Next
+	// } else {
+	// 	return newHead
+	// }
 }
 
-func remove(node *ListNode, n int) (*ListNode, int) {
+func removeRecursively(node *ListNode, n int) (*ListNode, int) {
 	if node != nil {
-		_, m := remove(node.Next, n)
+		_, m := removeRecursively(node.Next, n)
 		if n == m {
 			node.Next = node.Next.Next
 		}
@@ -29,4 +30,33 @@ func remove(node *ListNode, n int) (*ListNode, int) {
 	} else {
 		return nil, 0
 	}
+}
+
+func removeIteratively(head *ListNode, n int) *ListNode {
+	i := 0
+	m := length(head)
+	node := head
+	var prevNode *ListNode
+	for node != nil {
+		if i == m-n {
+			if prevNode != nil {
+				prevNode.Next = node.Next
+			} else {
+				head = node.Next
+			}
+		}
+		prevNode = node
+		node = node.Next
+		i++
+	}
+	return head
+}
+
+func length(node *ListNode) int {
+	n := 0
+	for node != nil {
+		node = node.Next
+		n++
+	}
+	return n
 }
