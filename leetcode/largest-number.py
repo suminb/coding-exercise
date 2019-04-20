5cd833b7d074ac3a888d16e49c4173ccdd86dcd5
Sumin Byeon <suminb@gmail.com>
2019-04-21 05:43:26 +0900

    Accepted solution

--- leetcode/largest-number.py
+++ leetcode/largest-number.py
@@ -2,30 +2,43 @@
 # difficulty: medium
 # https://leetcode.com/problems/largest-number/
 
-from functools import partial
 from typing import List
 
 import pytest
 
 
+class CustomString(str):
+    def __gt__(self, other):
+        return self + other > other + self
+
+    def __lt__(self, other):
+        return self + other < other + self
+
+
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        n = len(nums)
-        return ''.join(
-            sorted([str(x) for x in nums], key=partial(key, n=n), reverse=True))
+        number = ''.join(sorted([CustomString(x) for x in nums], reverse=True))
+
+        while len(number) > 1 and number[0] == '0':
+            number = number[1:]
+
+        return number
 
 
-def key(x: str, n: int):
-    """We would like to compare numbers as if there is some sort of place
-    holders.
-    """
-    return x + '0' * (n - len(x))
+def test_compare():
+    assert CustomString('12') > CustomString('121')
+    assert CustomString('12') > CustomString('120')
+    assert CustomString('122') > CustomString('121')
+    assert CustomString('121') > CustomString('120')
 
 
 @pytest.mark.parametrize('nums, expected', [
     ([], ''),
     ([5], '5'),
+    ([0, 0], '0'),
+    ([0, 0, 0], '0'),
     ([10, 2], '210'),
+    ([10, 0, 0], '1000'),
     ([121, 12], '12121'),
     ([[3, 30, 34, 5, 9], '9534330']),
     ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], '9876543210'),
