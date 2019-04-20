3ffa6040ece27925f3850ba610e8f4a50fd64ffb
Sumin Byeon <suminb@gmail.com>
2019-04-21 05:01:02 +0900

    This one produces correct results, but way too slow

--- leetcode/largest-number.py
+++ leetcode/largest-number.py
@@ -9,12 +9,28 @@ import pytest
 
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        return ''.join(sorted([str(x) for x in nums], reverse=True))
+        return largest_number(nums)
+
+
+def largest_number(nums):
+    if len(nums) < 2:
+        return ''.join([str(x) for x in nums])
+    else:
+        return max(expand(nums))
+
+def expand(nums):
+    for i, x in enumerate(nums):
+        rest = nums[:i] + nums[i+1:]
+        yield str(x) + largest_number(rest)
 
 
 @pytest.mark.parametrize('nums, expected', [
-    ([10,2], '210'),
-    ([[3,30,34,5,9], '9534330'])
+    ([], ''),
+    ([5], '5'),
+    ([10, 2], '210'), 
+    ([[3, 30, 34, 5, 9], '9534330']),
+    ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], '9876543210'),
+    ([93, 5, 3, 1, 3, 412, 45, 6151823579123, 3752], '9361518235791235454123752331'),
 ])
 def test(nums, expected):
     s = Solution()
