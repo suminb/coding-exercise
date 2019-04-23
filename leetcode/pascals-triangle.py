5771981d766c8eaedf9b2192211078928020dd14
Sumin Byeon <suminb@gmail.com>
2019-04-23 13:22:35 +0900

    Write some test cases

--- leetcode/pascals-triangle.py
+++ leetcode/pascals-triangle.py
@@ -4,6 +4,8 @@
 
 from typing import List
 
+import pytest
+
 
 class Solution:
     def generate(self, n: int) -> List[List[int]]:
@@ -19,3 +21,16 @@ class Solution:
 
 def generate(prev):
     return [x + y for x, y in zip([0] + prev, prev + [0])]
+
+
+@pytest.mark.parametrize('value, expected', [
+    ([1], [1, 1]),
+    ([1, 1], [1, 2, 1]),
+    ([1, 2, 1], [1, 3, 3, 1]),
+])
+def test(value, expected):
+    assert expected == generate(value)
+
+
+if __name__ == '__main__':
+    pytest.main(['-v', __file__])
