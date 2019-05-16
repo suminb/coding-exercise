a1bca7e9cea6887a2c9c1cc92062e0481022adf0
Sumin Byeon (SB) <suminb@gmail.com>
2019-05-16 18:41:18 +0900

    Getting through SAML

--- goex/src/awsauth/awsauth.go
+++ goex/src/awsauth/awsauth.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
 	"os"
 
+	"github.com/PuerkitoBio/goquery"
 	"golang.org/x/net/publicsuffix"
 )
 
 func main() {
-	authUrl := os.Getenv("AWS_AUTH_URL")
+	authURL := os.Getenv("AWS_AUTH_URL")
+	username := os.Getenv("AWS_USERNAME")
+	password := os.Getenv("AWS_PASSWORD")
 
-	u, err := url.Parse(authUrl)
+	parsedURL, err := url.Parse(authURL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,18 +31,62 @@ func main() {
 		Jar: jar,
 	}
 
-	resp, err := client.Get(u.String())
+	log.Printf("GET Request: %s\n", parsedURL)
+	resp, err := client.Get(parsedURL.String())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Printf("Header = %v\n", resp.Body)
-	fmt.Printf("Body = %s\n", body)
+	// body, err := ioutil.ReadAll(resp.Body)
+	// fmt.Printf("Header = %v\n", resp.Body)
+	// fmt.Printf("Body = %s\n", body)
 
-	fmt.Println("After 1st request:")
-	for _, cookie := range jar.Cookies(u) {
-		fmt.Printf("  %s: %s\n", cookie.Name, cookie.Value)
+	// fmt.Println("After 1st request:")
+	// for _, cookie := range jar.Cookies(parsedURL) {
+	// 	fmt.Printf("  %s: %s\n", cookie.Name, cookie.Value)
+	// }
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	loginForm := doc.Find("form#login")
+	actionPath, _ := loginForm.Attr("action")
+	actionURL := fmt.Sprintf("%s://%s%s", parsedURL.Scheme, parsedURL.Host, actionPath)
+
+	log.Printf("POST Request: %s", actionURL)
+	postData := make(url.Values)
+	postData["j_username"] = []string{username}
+	postData["j_password"] = []string{password}
+	resp, err = client.PostForm(actionURL, postData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc, err = goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	samlForm := doc.Find("form")
+	samlResp := samlForm.Find("input[name=SAMLResponse]")
+	samlRespValue, _ := samlResp.Attr("value")
+	actionURL, _ = samlForm.Attr("action")
+
+	log.Printf("POST Request: %s", actionURL)
+	postData = make(url.Values)
+	postData["SAMLResponse"] = []string{samlRespValue}
+	resp, err = client.PostForm(actionURL, postData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc, err = goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	html, _ := doc.Html()
+	fmt.Println(html)
 }
