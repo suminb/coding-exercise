beb7f264282bd7843ce0da6887a3badd5f515865
Sumin Byeon (SB) <suminb@gmail.com>
2019-05-17 12:27:06 +0900

    Code refactoring

--- goex/src/awsauth/awsauth.go
+++ goex/src/awsauth/awsauth.go
@@ -1,92 +1,139 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
 	"os"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"golang.org/x/net/publicsuffix"
+	"golang.org/x/text/encoding"
 )
 
-func main() {
-	authURL := os.Getenv("AWS_AUTH_URL")
-	username := os.Getenv("AWS_USERNAME")
-	password := os.Getenv("AWS_PASSWORD")
-
-	parsedURL, err := url.Parse(authURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func MakeSessionedClient() (*http.Client, error) {
 	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	client := &http.Client{
 		Jar: jar,
 	}
+	return client, nil
+}
+
+// GetReaderFromFile returns a reader object from a file
+func GetReaderFromFile(filename string) io.Reader {
+	file, _ := os.Open(filename)
+	return bufio.NewReader(file)
+}
+
+func ForceConvertEncoding(reader *io.Reader, encoding encoding.Encoding) io.Reader {
+	decoder := encoding.NewDecoder()
+	return decoder.Reader(*reader)
+}
+
+func FetchLoginForm(client *http.Client, loginURL string) (*http.Response, error) {
+	parsedURL, err := url.Parse(loginURL)
+	if err != nil {
+		return nil, err
+	}
 
 	log.Printf("GET Request: %s\n", parsedURL)
 	resp, err := client.Get(parsedURL.String())
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
+	return resp, nil
+}
 
-	// body, err := ioutil.ReadAll(resp.Body)
-	// fmt.Printf("Header = %v\n", resp.Body)
-	// fmt.Printf("Body = %s\n", body)
+func PostAuthForm(client *http.Client, loginDoc *goquery.Document, loginURL string) (*http.Response, error) {
+	// FIXME: This is a temporary workaround
+	username := os.Getenv("AWS_USERNAME")
+	password := os.Getenv("AWS_PASSWORD")
 
-	// fmt.Println("After 1st request:")
-	// for _, cookie := range jar.Cookies(parsedURL) {
-	// 	fmt.Printf("  %s: %s\n", cookie.Name, cookie.Value)
-	// }
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	loginForm := doc.Find("form#login")
+	parsedURL, _ := url.Parse(loginURL)
+	loginForm := loginDoc.Find("form#login")
 	actionPath, _ := loginForm.Attr("action")
 	actionURL := fmt.Sprintf("%s://%s%s", parsedURL.Scheme, parsedURL.Host, actionPath)
 
-	log.Printf("POST Request: %s", actionURL)
 	postData := make(url.Values)
 	postData["j_username"] = []string{username}
 	postData["j_password"] = []string{password}
-	resp, err = client.PostForm(actionURL, postData)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	doc, err = goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	samlForm := doc.Find("form")
-	samlResp := samlForm.Find("input[name=SAMLResponse]")
-	samlRespValue, _ := samlResp.Attr("value")
-	actionURL, _ = samlForm.Attr("action")
 
 	log.Printf("POST Request: %s", actionURL)
-	postData = make(url.Values)
+	return client.PostForm(actionURL, postData)
+}
+
+func PostSamlForm(client *http.Client, authDoc *goquery.Document) (*http.Response, error) {
+	samlForm := authDoc.Find("form")
+	samlResp := samlForm.Find("input[name=SAMLResponse]")
+	samlRespValue, _ := samlResp.Attr("value")
+	actionURL, _ := samlForm.Attr("action")
+
+	postData := make(url.Values)
 	postData["SAMLResponse"] = []string{samlRespValue}
-	resp, err = client.PostForm(actionURL, postData)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	doc, err = goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Printf("POST Request: %s", actionURL)
+	return client.PostForm(actionURL, postData)
+}
 
-	html, _ := doc.Html()
+// ProcessSamlForm is a temporary workaround to deal with malformed HTML. We are
+// going to need a more robust solution.
+func ProcessSamlForm(reader io.Reader) io.Reader {
+	samlContentBuf, _ := ioutil.ReadAll(reader)
+	samlContent := string(samlContentBuf)
+	start := strings.Index(samlContent, "<div id=\"container\">")
+	end := strings.Index(samlContent, "</body>")
+	return strings.NewReader(samlContent[start:end])
+}
+
+func GetRoles(samlDoc *goquery.Document) []string {
+	html, _ := samlDoc.Html()
 	fmt.Println(html)
+	roles := make([]string, 0)
+	inputFields := samlDoc.Find("input[name=roleIndex]")
+	inputFields.Each(func(i int, s *goquery.Selection) {
+		value, _ := s.Attr("value")
+		roles = append(roles, value)
+	})
+	return roles
+}
+
+func NewDocumentFromReader(reader io.Reader) (*goquery.Document, error) {
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		return nil, err
+	}
+	return doc, nil
+}
+
+func main() {
+	loginURL := os.Getenv("AWS_AUTH_URL")
+	client, _ := MakeSessionedClient()
+
+	// Redirect to the auth form
+	loginFormResp, _ := FetchLoginForm(client, loginURL)
+	loginDoc, _ := NewDocumentFromReader(loginFormResp.Body)
+
+	// Authentication
+	authResp, _ := PostAuthForm(client, loginDoc, loginURL)
+	authDoc, _ := NewDocumentFromReader(authResp.Body)
+
+	// Choose a role
+	samlResp, _ := PostSamlForm(client, authDoc)
+	samlDoc, _ := NewDocumentFromReader(ProcessSamlForm(samlResp.Body))
+
+	// reader := GetReaderFromFile("saml.html")
+	// samlDoc, _ := NewDocumentFromReader(ProcessSamlForm(reader))
+
+	roles := GetRoles(samlDoc)
+	fmt.Println(roles)
 }
