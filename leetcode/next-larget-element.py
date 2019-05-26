48f7ff2ef49d0aba0d7df7411bfd2a5224482649
Sumin Byeon <suminb@gmail.com>
2019-05-26 22:52:13 +0800

    Next largest element (brute-force solution)

---
+++ leetcode/next-larget-element.py
@@ -0,0 +1,32 @@
+# This is not from LeetCode
+# https://www.geeksforgeeks.org/next-greater-element/
+
+import pytest
+
+from typing import List
+
+
+def next_greater_element(xs: List[int]) -> List[int]:
+    """NOTE: This is a brute-force solution."""
+    n = len(xs)
+    j = 0
+    res = []
+    for i, curr in enumerate(xs):
+        j = i
+        while j < n and xs[j] <= curr:
+            j += 1
+        res.append(xs[j] if j < n else -1)
+
+    return res
+
+
+@pytest.mark.parametrize('xs, expected', [
+    ([4, 5, 2, 25], [5, 25, 25, -1]),
+    ([13, 7, 6, 12], [-1, 12, 12, -1]),
+])
+def test_next_greater_element(xs, expected):
+    actual = next_greater_element(xs)
+    assert expected == actual
+
+if __name__ == '__main__':
+    pytest.main(['-v', __file__])
