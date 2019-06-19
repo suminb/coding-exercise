04b312cba59f3cc51e95b8945dfbc188e7509761
Sumin Byeon <suminb@gmail.com>
2019-06-19 18:33:16 +0900

    Implement cartesian product

--- leetcode/letter_combinations_of_a_phone_number.py
+++ leetcode/letter_combinations_of_a_phone_number.py
@@ -4,7 +4,7 @@
 # [17] Letter Combinations of a Phone Number
 # https://leetcode.com/problems/letter-combinations-of-a-phone-number/submissions/
 
-from itertools import product
+from functools import reduce
 from typing import List
 
 import pytest
@@ -26,13 +26,29 @@ class Solution:
         if not digits:
             return []
         candidates = [self.table[d] for d in digits]
-        # FIXME: Could we do this without itertools.product()?
-        return [''.join(p) for p in product(*candidates)]
+        return [''.join(p) for p in cartesian_product(*candidates)]
+
+
+def cartesian_product_of_two(xs, ys):
+    for x in xs:
+        for y in ys:
+            if isinstance(x, tuple):
+                yield (*x, y)
+            else:
+                yield (x, y)
+
+
+def cartesian_product(*sets):
+    """NOTE: We could've used itertools.product(), but it is worth implementing
+    this ourselves for educational purposes.
+    """
+    return reduce(lambda x, y: cartesian_product_of_two(x, y), sets)
 
 
 @pytest.mark.parametrize('digits, expected', [
     ('', []),
     ('23', ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']),
+    # TODO: We need more test cases
 ])
 def test_letter_combinations(digits, expected):
     actual = Solution().letterCombinations(digits)
