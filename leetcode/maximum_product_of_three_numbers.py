9a57bd150dc4ba18b1eea218c8a3eaf2013b9a82
Sumin Byeon <suminb@gmail.com>
2019-07-15 17:00:49 +0100

    More efficient solution

--- leetcode/maximum_product_of_three_numbers.py
+++ leetcode/maximum_product_of_three_numbers.py
@@ -23,13 +23,39 @@ def product(nums):
     return reduce(lambda x, y: x * y, nums)
 
 
+def maximum_product_onepass(nums):
+    max1 = max2 = max3 = -1000
+    min1 = min2 = 1000
+
+    for x in nums:
+        if x < min1:
+            min2 = min1
+            min1 = x
+        elif x < min2:
+            min2 = x
+        if x > max3:
+            max1 = max2
+            max2 = max3
+            max3 = x
+        elif x > max2:
+            max1 = max2
+            max2 = x
+        elif x > max1:
+            max1 = x
+
+    return max(max1 * max2 * max3, min1 * min2 * max3)
+
+
 @pytest.mark.parametrize('nums, expected', [
     ([1, 2, 3], 6),
-    ([1, 2, 3, 4], 24), 
+    ([1, 2, 3, 4], 24),
     ([1, 2, 3, 7, 9, -1, -13, -11], 1287), 
 ])
 def test_maximum_product(nums, expected):
     actual = maximum_product_by_sorting(nums)
+    assert expected == actual
+
+    actual = maximum_product_onepass(nums)
     assert expected == actual
 
 
